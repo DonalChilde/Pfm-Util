c7353b5971d05b8dad9fb174b65770099d9ca75e
Chad <chad@pop-os.localdomain>
2020-09-26 09:31:04 -0700

    copied code from pbs_parser, pdf to text and text chuk parser

---
+++ src/pfm_util/file_utilities/pdf_text_grab.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+from PyPDF4 import PdfFileReader
+from typing import Tuple, Optional, List, Callable
+from dataclasses import dataclass
+
+
+@dataclass
+class PageContent:
+    page_number: int
+    page_text: str
+
+
+def get_page_content(
+    file_path: Path,
+    start_range: int = 0,
+    end_range: int = -1,
+    start_filter: Optional[Callable[[int, str], bool]] = None,
+    end_filter: Optional[Callable[[int, str], bool]] = None,
+    content_filter: Optional[Callable[[int, str], bool]] = None,
+) -> List[PageContent]:
+    """Get the text from a pdf file.
+
+    Get the text from each page of a pdf file, with optional limits to page range, and flag filters for 
+    start, end, and page content. Returns a PageContent containing page_number and page_text.
+    
+    Arguments:
+        file_path {Path} -- [description]
+    
+    Keyword Arguments:
+        start_range {int} -- [description] (default: {0})
+        end_range {int} -- [description] (default: {-1})
+        start_filter {Optional[Callable[[int, str], bool]]} -- [description] (default: {None})
+        end_filter {Optional[Callable[[int, str], bool]]} -- [description] (default: {None})
+        content_filter {Optional[Callable[[int, str], bool]]} -- [description] (default: {None})
+    
+    Raises:
+        ValueError: [description]
+        ValueError: [description]
+    
+    Returns:
+        List[PageContent] -- [description]
+    """
+
+    def pass_through_true(page_number: int, page_content: str):
+        return True
+
+    def pass_through_false(page_number: int, page_content: str):
+        return False
+
+    if start_filter is None:
+        start_filter = pass_through_true
+    if end_filter is None:
+        end_filter = pass_through_false
+    if content_filter is None:
+        content_filter = pass_through_true
+
+    retval: List[PageContent] = []
+    start_flag = False
+
+    with open(file_path, "rb") as f:
+        read_pdf = PdfFileReader(f)
+        number_of_pages = read_pdf.getNumPages()
+        if number_of_pages is None:
+            raise ValueError(f"No pages found for {file_path}")
+
+        if end_range == -1 or end_range > number_of_pages:
+            end_range = int(number_of_pages)
+        if start_range > end_range:
+            raise ValueError(
+                f"{start_range} is bigger than {end_range}. Cannot count backwards."
+            )
+
+        for x in range(start_range, end_range):
+            page = read_pdf.getPage(x)
+            page_text = page.extractText()
+            if not start_flag:
+                if start_filter(x, page_text):
+                    retval.append(PageContent(x, page_text))
+                    start_flag = True
+                    continue
+                continue
+            if end_filter(x, page_text):
+                retval.append(PageContent(x, page_text))
+                break
+            if content_filter(x, page_text):
+                retval.append(PageContent(x, page_text))
+                continue
+    return retval
+
---
+++ src/pfm_util/parsing/text_chunk_parser.py
@@ -0,0 +1,177 @@
+from dataclasses import dataclass
+from typing import Any, Callable, Dict, List, Optional, Sequence, Tuple
+
+from pyparsing import ParseException, ParserElement, ParseResults
+
+
+# pylint: disable=invalid-name
+@dataclass
+class ChunkParser:
+    chunk_definition: ParserElement
+    expected_next: List[str]
+    result_handler: Any
+    # result_handler: Callable[
+    #     [dict, Optional[ParseResults], Optional[str], Optional["TextChunk"]], dict
+    # ]
+
+
+# @dataclass_json
+@dataclass
+class ParseMessage:
+    category: str = ""
+    message: str = ""
+    text_chunk = Optional["TextChunk"]
+    data: str = ""
+
+
+@dataclass
+class TextChunk:
+    chunk_id: str = ""
+    name: str = ""
+    text_chunk: str = ""
+    chunk_id_key: str = ""
+
+    def __str__(self):
+        return (
+            f"[{self.chunk_id} - {self.name}] [{self.chunk_id_key}] {self.text_chunk}"
+        )
+
+
+# TODO consider taking chunk_id out of result handler signature, as it is duplicated inside chunk.
+# TODO consider making the result handler an ABCclass, to guarantee signature of handler.
+# TODO define schemes as subclass of ChunkParser. Make class of ChunkParser that represents
+#   state, and assumes no parse, eg. Origin?
+# TODO define parse framework, split from pyparsing. userdefine parser in subclasses.
+# TODO allow custom chunker
+# TODO pass definable look ahead chunks and look behind chunks to parser.
+
+
+def advance_parse_position(line_id: str) -> bool:
+    if line_id not in ["UNKNOWN", "BLANK"]:
+        return True
+    else:
+        return False
+
+
+class TextChunkParser:
+    """
+    parse scheme reserved keys: "ORIGIN","START","END","TERMINUS"
+    parse context reserved keys: "PARSE_RESULT"
+    """
+
+    def __init__(
+        self,
+        parse_scheme: Dict[str, ChunkParser],
+        check_parse_advance: Optional[Callable[[str], bool]] = None,
+    ):
+        if check_parse_advance:
+            self.check_parse_advance = check_parse_advance
+        else:
+            self.check_parse_advance = self.advance_parse_position
+        self.parse_scheme = parse_scheme
+
+    def identify_chunks(
+        self,
+        chunks: Sequence[Sequence[TextChunk]],
+        parse_context: Optional[Dict[str, Any]] = None,
+    ) -> Sequence[Sequence[TextChunk]]:
+        if not parse_context:
+            parse_context = dict()
+        parse_position = "ORIGIN"
+        # parse_context = self.parse_scheme[parse_position].result_handler(
+        #     parse_context, None, None, None
+        # )
+        for chunk_list in chunks:
+            parse_position = "START"
+            # parse_context = self.parse_scheme[parse_position].result_handler(
+            #     parse_context, None, None, None
+            # )
+            for chunk in chunk_list:
+                expected_keys = self.parse_scheme[parse_position].expected_next
+                result = self.parse_attempt(expected_keys, chunk)
+                if not result:
+                    print(f"Broken: could not parse {chunk.text_chunk}")
+                    # TODO better error handling and messaging
+                    raise NotImplementedError
+                chunk_id_key, parse_result = result
+                chunk.chunk_id_key = chunk_id_key
+                if self.check_parse_advance(chunk_id_key):
+                    parse_position = chunk_id_key
+                # parse_context = self.parse_scheme[chunk_id_key].result_handler(
+                #     parse_context, parse_result, chunk_id_key, chunk
+                # )
+            parse_position = "END"
+            # parse_context = self.parse_scheme[parse_position].result_handler(
+            #     parse_context, None, None, None
+            # )
+        parse_position = "TERMINUS"
+        # parse_context = self.parse_scheme[parse_position].result_handler(
+        #     parse_context, None, None, None
+        # )
+        return chunks
+
+    def parse_chunks(
+        self,
+        chunks: Sequence[Sequence[TextChunk]],
+        parse_context: Optional[Dict[str, Any]] = None,
+    ) -> Any:
+        """
+        Identify chunks and pass them to the associated handler.
+        there are four positional handlers to enable more fine grained
+        handling of position related parsing.
+        ORIGIN: Before any parsing is attempted.
+        START: The beginning of a list of TextChunks, before parsing.
+        END: The end of a list of TextChunks
+        TERMINUS: After all parsing is complete.
+
+        The parseContext dict is passed to each result handler, and can be used to
+        track state, and contain objects derived from the parsed text.
+        """
+        if not parse_context:
+            parse_context = dict()
+        parse_position = "ORIGIN"
+        parse_context = self.parse_scheme[parse_position].result_handler(
+            parse_context, None, None, None
+        )
+        for chunk_list in chunks:
+            parse_position = "START"
+            parse_context = self.parse_scheme[parse_position].result_handler(
+                parse_context, None, None, None
+            )
+            for chunk in chunk_list:
+                expected_keys = self.parse_scheme[parse_position].expected_next
+                result = self.parse_attempt(expected_keys, chunk)
+                if not result:
+                    print(f"Broken: could not parse {chunk.text_chunk}")
+                    # TODO better error handling and messaging
+                    raise NotImplementedError
+                chunk_id_key, parse_result = result
+                chunk.chunk_id_key = chunk_id_key
+                if self.check_parse_advance(chunk_id_key):
+                    parse_position = chunk_id_key
+                parse_context = self.parse_scheme[chunk_id_key].result_handler(
+                    parse_context, parse_result, chunk_id_key, chunk
+                )
+            parse_position = "END"
+            parse_context = self.parse_scheme[parse_position].result_handler(
+                parse_context, None, None, None
+            )
+        parse_position = "TERMINUS"
+        parse_context = self.parse_scheme[parse_position].result_handler(
+            parse_context, None, None, None
+        )
+        return parse_context  # type: ignore
+
+    def parse_attempt(self, key_list: Sequence[str], text_chunk: TextChunk):
+        for key in key_list:
+            try:
+                result = self.parse_scheme[key].chunk_definition.parseString(
+                    text_chunk.text_chunk
+                )
+                return (key, result)
+            except ParseException:
+                continue
+        return None
+
+    def advance_parse_position(self, chunk_id_key: str) -> bool:
+        return chunk_id_key not in ["UNKNOWN", "BLANK"]
